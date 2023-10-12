ff81f8e692c62cf154938c3104c4202aa4eb20e0
matiasrees <matiasrees@gmail.com>
2023-10-12 17:41:26 -0300

    2da-entrega-final

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-alert("¡Bienvenido al sistema de arriendo de canchas!")
+alert("¡Bienvenido al sistema de arriendo de canchas!");
+
 const reservas = {};
 
-function mostrarmenu() {
+function mostrarMenu() {
     let opcion;
     do {
-        opcion = prompt("Selecciona una opción:\n\n1. Reservar canchas\n2. Mostrar reservas\n3. Modificar reserva\n4. Cancelar reserva\n5. Salir");
+        opcion = prompt(
+            "Selecciona una opción:\n\n1. Reservar canchas\n2. Mostrar reservas\n3. Modificar reserva\n4. Cancelar reserva\n5. Salir"
+        );
 
         switch (opcion) {
             case "1":
-                reservarcanchas();
+                reservarCanchas();
                 break;
             case "2":
-                mostrarreservas();
+                mostrarReservas();
                 break;
             case "3":
-                modificarreserva();
+                modificarReserva();
                 break;
             case "4":
-                cancelarreserva();
+                cancelarReserva();
                 break;
             case "5":
                 alert("¡Hasta luego! Vuelve pronto.");
@@ -28,59 +31,69 @@ function mostrarmenu() {
     } while (opcion !== "5");
 }
 
-function mostrarhorarios() {
-    const horarios = ['09:00 a 10:00', '11:00 a 12:00', '13:00 a 14:00', '15:00 a 16:00', '17:00 a 18:00', '19:00 a 20:00', '21.00 a 22:00'];
-    let horarioslistado = "Selecciona un horario:\n\n";
+function mostrarHorarios() {
+    const horarios = [
+        "09:00 a 10:00",
+        "11:00 a 12:00",
+        "13:00 a 14:00",
+        "15:00 a 16:00",
+        "17:00 a 18:00",
+        "19:00 a 20:00",
+        "21.00 a 22:00"
+    ];
+
+    let horariosListado = "Selecciona un horario:\n\n";
     for (let i = 0; i < horarios.length; i++) {
-        horarioslistado += `${i + 1}. ${horarios[i]}\n`;
+        horariosListado += `${i + 1}. ${horarios[i]}\n`;
     }
-    const seleccionhorario = parseInt(prompt(horarioslistado));
-    if (seleccionhorario >= 1 && seleccionhorario <= horarios.length) {
-        return horarios[seleccionhorario - 1];
+
+    const seleccionHorario = parseInt(prompt(horariosListado));
+    if (seleccionHorario >= 1 && seleccionHorario <= horarios.length) {
+        return horarios[seleccionHorario - 1];
     } else {
         alert("Opción de horario inválida. Por favor, selecciona un horario válido.");
-        return mostrarhorarios();
+        return mostrarHorarios();
     }
 }
 
-function reservarcanchas() {
-    const horarioseleccionado = mostrarhorarios();
-    const numeroreserva = Object.keys(reservas).length + 1;
-    reservas[numeroreserva] = horarioseleccionado;
-    alert(`Has reservado una cancha para el horario ${horarioseleccionado}. Número de reserva: ${numeroreserva}`);
+function reservarCanchas() {
+    const horarioSeleccionado = mostrarHorarios();
+    const numeroReserva = Object.keys(reservas).length + 1;
+    reservas[numeroReserva] = horarioSeleccionado;
+    alert(`Has reservado una cancha para el horario ${horarioSeleccionado}. Número de reserva: ${numeroReserva}`);
 }
 
-function mostrarreservas() {
-    let listareservas = "Reservas:\n\n";
-    for (const numeroreserva in reservas) {
-        listareservas += `Reserva #${numeroreserva}: ${reservas[numeroreserva]}\n`;
+function mostrarReservas() {
+    let listaReservas = "Reservas:\n\n";
+    for (const numeroReserva in reservas) {
+        listaReservas += `Reserva #${numeroReserva}: ${reservas[numeroReserva]}\n`;
     }
-    alert(listareservas);
+    alert(listaReservas);
 }
 
-function modificarreserva() {
-    mostrarreservas();
-    const numeroreservamodificar = parseInt(prompt("Introduce el número de reserva que deseas modificar:\n"));
-    if (reservas[numeroreservamodificar]) {
-        const nuevohorario = mostrarhorarios();
-        reservas[numeroreservamodificar] = nuevohorario;
-        alert(`Reserva #${numeroreservamodificar} modificada. Nuevo horario: ${nuevohorario}`);
+function modificarReserva() {
+    mostrarReservas();
+    const numeroReservaModificar = parseInt(prompt("Introduce el número de reserva que deseas modificar:\n"));
+
+    if (reservas[numeroReservaModificar]) {
+        const nuevoHorario = mostrarHorarios();
+        reservas[numeroReservaModificar] = nuevoHorario;
+        alert(`Reserva #${numeroReservaModificar} modificada. Nuevo horario: ${nuevoHorario}`);
     } else {
         alert("Número de reserva inválido. Por favor, introduce un número de reserva válido.");
     }
 }
 
-function cancelarreserva() {
-    mostrarreservas();
-    const numeroreservacancelar = parseInt(prompt("Introduce el número de reserva que deseas cancelar:\n"));
+function cancelarReserva() {
+    mostrarReservas();
+    const numeroReservaCancelar = parseInt(prompt("Introduce el número de reserva que deseas cancelar:\n"));
 
-    if (reservas[numeroreservacancelar]) {
-        delete reservas[numeroreservacancelar];
-        alert(`Reserva #${numeroreservacancelar} cancelada correctamente.`);
+    if (reservas[numeroReservaCancelar]) {
+        delete reservas[numeroReservaCancelar];
+        alert(`Reserva #${numeroReservaCancelar} cancelada correctamente.`);
     } else {
         alert("Número de reserva inválido. Por favor, introduce un número de reserva válido.");
     }
 }
 
-mostrarmenu();
-
+mostrarMenu();
